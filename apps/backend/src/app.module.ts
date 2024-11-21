fa71b3ad77c4ab574ff9d376bf29ba3692549a25
Livia Brandao <liviatoledo27@gmail.com>
2024-11-21 15:01:15 -0300

    search for events or aliases

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EventosModule } from './eventos/eventos.module';
+import { EventosController } from './eventos/eventos.controller';
 
 @Module({
   imports: [EventosModule],
-  controllers: [AppController],
+  controllers: [EventosController],
   providers: [],
 })
 export class AppModule {}

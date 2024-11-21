a7685888904f0667b13e5653942aa89564675a5e
Livia Brandao <liviatoledo27@gmail.com>
2024-11-21 15:34:48 -0300

    adding a new guest

--- apps/backend/src/eventos/eventos.controller.ts
+++ apps/backend/src/eventos/eventos.controller.ts
@@ -1,8 +1,22 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { Data, Evento, eventos, Id } from 'core';
+import { Convidado, Data, Evento, eventos, Id } from 'core';
 
 @Controller('eventos')
 export class EventosController {
+  @Post(':alias/convidados')
+  async salvarConvidado(
+    @Param('alias') alias: string,
+    @Body() convidado: Convidado,
+  ) {
+    const evento = eventos.find((evento) => evento.alias === alias);
+    if (!evento) {
+      throw new Error('Evento não encontrado!');
+    }
+
+    evento.convidados.push(convidado);
+    return this.serializar(evento);
+  }
+
   @Post('acessar')
   async acessarEvento(@Body() dados: { id: string; senha: string }) {
     const evento = eventos.find(

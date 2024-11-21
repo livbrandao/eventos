dbff2cfc3389c315e19c88a250adebd60f8035fa
Livia Brandao <liviatoledo27@gmail.com>
2024-11-21 01:46:02 -0300

    configuring and entering data into the database

--- packages/core/src/shared/Id.ts
+++ packages/core/src/shared/Id.ts
@@ -1,9 +1,6 @@
+import { v4 as uuid } from "uuid";
 export default class Id {
   static novo(): string {
-    return `${this.hash()}-${this.hash()}-${this.hash()}`;
-  }
-
-  private static hash(): string {
-    return Math.random().toString(36).substring(2, 15);
+    return uuid();
   }
 }

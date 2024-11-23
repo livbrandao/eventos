57f2530c9e5a66ff77662f5e2f6464be0ac77714
Livia Brandao <liviatoledo27@gmail.com>
2024-11-23 13:58:01 -0300

    insert component logo

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import Logo from "@/components/template/Logo";
 import { bgBlack, centerGrow, flex1 } from "@/style";
 import { ImageBackground, SafeAreaView, Text } from "react-native";
 
@@ -7,6 +8,8 @@ export default function Index() {
       source={require("@/assets/images/background.png")}
       resizeMode="cover"
       style={[flex1, bgBlack]}
-    ></ImageBackground>
+    >
+      <Logo />
+    </ImageBackground>
   );
 }

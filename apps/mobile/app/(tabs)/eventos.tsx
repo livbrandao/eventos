31b816a512344d269f23fbdb29cccf965f59bfb9
Livia Brandao <liviatoledo27@gmail.com>
2024-11-23 13:37:52 -0300

    tabbed navigation

---
+++ apps/mobile/app/(tabs)/eventos.tsx
@@ -0,0 +1,10 @@
+import { centerGrow, flex1 } from "@/style";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={[flex1, centerGrow]}>
+      <Text style={[]}>Eventos</Text>
+    </View>
+  );
+}
--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { centerGrow, flex1 } from "@/style";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View style={[flex1, centerGrow]}>
+    <SafeAreaView style={[flex1, centerGrow]}>
       <Text style={[]}>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    </SafeAreaView>
   );
 }
